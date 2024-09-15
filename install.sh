12892ba3c05b2807ae802f3107ba1462cd666df1
declspecl <gavind2559@gmail.com>
2024-09-15 14:16:47 -0400

    create install script

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo pacman -S zsh hyprland hyprpaper hypridle xdg-desktop-portal-hyprland xdg-desktop-portal-gtk kitty mako neovim waybar wofi pavucontrol
+yay -S hyprpicker
+yay -S wlogout
